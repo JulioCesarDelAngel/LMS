6087b25173dc0e5fb1033d9276adc2f7b23eb4b7
MBA Julius Mathison <juliusmathison@MacBook-Air-de-Julius.local>
2024-10-05 15:13:50 -0600

    se formatea formulario de contacto

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,73 +1,77 @@
-import React from "react";
+import React, { useState , useEffect} from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 
-
 export default function Contact(){
+    const formId = process.env.REACT_APP_FORMSPREE_ID;    
+    const [state, handleSubmit] = useForm({formId});
+    const [userFormData, setUserFormData] = useState({ email: '', message: '' });
+    
+    // useEffect(() => {
+    //     console.log('DeBuggsBunny:', userFormData);
+    //   }, [userFormData]);
 
-    const [state, handleSubmit] = useForm("mjkvbvjj");
+
     if (state.succeeded) {
         return <p>Gracias por contactarnos!</p>;
     }
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+        console.log(name,value);
+        console.log('el email',userFormData.email);
+      };
+
+
     return (
-        <div className="container text-center align-middle">
-        <h3 className="text-center">Contactanos</h3>
 
-        <form onSubmit={handleSubmit}>
-            <fieldset>
-            <div>
-            <label for="email" class="form-label mt-4">Dirección de correo electrónico</label>
-            <input type="email" class="form-control" id="email" name="email"aria-describedby="emailHelp" placeholder="Introducir correo electrónico"/>
-            <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
-            <ValidationError 
-                prefix="Email" 
-                field="email"
-                errors={state.errors}
-            />
-            </div>
+        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <div class="col-md-8 col-lg-6">
+        <div className="container text-center align-middle" >
+            <h3 className="text-center">Contactanos</h3>
 
-            {/* <label htmlFor="email2">
-                Email Address
-            </label>
-            <input
-                id="email2"
-                type="email" 
-                name="email2"
-            />
-            <ValidationError 
-                prefix="Email" 
-                field="email2"
-                errors={state.errors}
-            /> */}
-
-            <div>
-
-            <label for="exampleTextarea" class="form-label mt-4">Capture su  mensaje</label>
-            <textarea class="form-control" id="message" name="message" rows="3"></textarea>
-            
-            <ValidationError 
-                prefix="Message" 
-                field="message"
-                errors={state.errors}
-            />
-            </div>
-            {/* <textarea
-                id="message"
-                name="message"
-            />
-            <ValidationError 
-                prefix="Message" 
-                field="message"
-                errors={state.errors}
-            /> */}
-            <div class="mt-5">
-                <button type="submit" class="btn btn-light m-t-5" disabled={state.submitting}>
-                    ENVIAR MENSAJE
-                </button>
-            </div>
-            </fieldset>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <div>
+                        <label for="email" class="form-label mt-4">Dirección de correo electrónico</label>
+                        <input type="email" class="form-control" id="email" name="email"aria-describedby="emailHelp" 
+                        placeholder="Introducir correo electrónico"
+                        autoComplete="off"
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                        />
+                        <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
+                        <ValidationError 
+                            prefix="Email" 
+                            field="email"
+                            errors={state.errors}
+                        />
+                    </div>
+                    <div>
+                        <label for="exampleTextarea" class="form-label mt-4">Capture su  mensaje</label>
+                        <textarea class="form-control" id="message" name="message" rows="4"
+                            onChange={handleInputChange}
+                            value={userFormData.message}
+                            required                        
+                        ></textarea>
+                        
+                        <ValidationError 
+                            prefix="Message" 
+                            field="message"
+                            errors={state.errors}
+                        />
+                    </div>
+                    <div class="mt-3">
+                        <button type="submit" class="btn btn-light m-t-5" disabled={ !(userFormData.email && userFormData.message) || (state.submitting) /**/  } >
+                            ENVIAR MENSAJE                            
+                        </button>
+                    </div>
+                </fieldset>
             </form>            
-    </div>
+         </div>
 
+         </div>
+         </div>
     );
 }
