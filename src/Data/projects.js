8c642ed34122421f634dd838cdafc471eed21445
MBA Julius Mathison <juliusmathison@MacBook-Air-de-Julius.local>
2024-10-08 19:37:20 -0600

    se agrega catalogo de apps

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -2,7 +2,7 @@ export const projects =[
     {
         titulo:"Baby Num",
         subtitulo:"Juego educativo para niños de 1-5 años",
-        imagen:"https://github.com/JulioCesarDelAngel/LMS/blob/main/src/assets/BabyNum.gif",
+        imagen:"https://github.com/JulioCesarDelAngel/LMS/raw/main/src/assets/BabyNum.gif",
         git:"https://github.com/JulioCesarDelAngel/LMS",
         link:"https://juliocesardelangel.github.io/LMS"
     },    
--- src/components/pages/Apps.js
+++ src/components/pages/Apps.js
@@ -10,19 +10,20 @@ export default function Apps () {
                 {
                     projects.map((project)=>(
                         <div className="col">
-                            <div className="card h-100">
-                            <div class="card-header bg-transparent border-info">
-                                <a className="card-title" href={project.link} target="_blank" rel="noreferrer">
-                                    {project.titulo}
-                                </a>
-                            </div>                            
+                            <div className="card h-50 w-50">
+                                <div class="card-header bg-transparent border-info">
+                                    <a className="card-title" href={project.link} target="_blank" rel="noreferrer">
+                                        {project.titulo}
+                                    </a>
+                                </div>                            
 
-                            <img src={project.imagen} className="card-img-top h-100" alt={project.titulo}/>
-                            <div class="card-footer bg-transparent border-info">
-                                <a className="card-text" href={project.git} target="_blank" rel="noreferrer">
-                                <i className="bi bi-github" style={{fontSize:"2rem",color: "white"}}></i>
-                                </a>                                
-                            </div>
+                                <img src={project.imagen} className="card-img-top h-100 w-100" alt={project.titulo} />
+
+                                <div class="card-footer bg-transparent border-info">
+                                    <a className="card-text" href={project.git} target="_blank" rel="noreferrer">
+                                    <i className="bi bi-github" style={{fontSize:"1rem",color: "white"}}></i>
+                                    </a>                                
+                                </div>
                             </div>
                         </div>
                     ))
