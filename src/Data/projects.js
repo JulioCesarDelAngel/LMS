5a1e36feb6b642c2c7614e29df67894a884b305e
MBA Julius Mathison <juliusmathison@MacBook-Air-de-Julius.local>
2024-10-10 11:46:01 -0600

    se agrega gif small a apps

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -2,7 +2,7 @@ export const projects =[
     {
         titulo:"Baby Num",
         subtitulo:"Juego educativo para niños de 1-5 años",
-        imagen:"https://github.com/JulioCesarDelAngel/LMS/raw/main/src/assets/BabyNum.gif",
+        imagen:"https://github.com/JulioCesarDelAngel/LMS/raw/main/src/assets/BabyNumSm.gif",
         git:"https://github.com/JulioCesarDelAngel/LMS",
         link:"https://juliocesardelangel.github.io/LMS"
     },    
